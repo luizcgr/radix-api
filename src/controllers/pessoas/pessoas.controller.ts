aa58a12880613328c0622fed37ea9cbc6245e47b
@luizcgr <luizcgr@gmail.com>
2025-12-28 03:03:46 +0000

    refactor: remove TransactionObserver decorator from PessoasController

--- src/controllers/pessoas/pessoas.controller.ts
+++ src/controllers/pessoas/pessoas.controller.ts
@@ -1,9 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
-import { TransactionObserver } from 'src/infra/database/transactions/transaction-observer';
 
 @Controller({ path: 'v1/pessoas' })
 export class PessoasController {
-  @TransactionObserver()
   @Get()
   heath() {
     return { status: 'ok' };
