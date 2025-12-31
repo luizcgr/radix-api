1d2560c017529c16832591b0ecf802ab16e7b16e
@luizcgr <luizcgr@gmail.com>
2025-12-31 11:50:20 +0000

    refactor: streamline webhook access token validation and payment processing logic

--- src/controllers/webhook/webhook.controller.ts
+++ src/controllers/webhook/webhook.controller.ts
@@ -7,7 +7,6 @@ import {
   UnauthorizedException,
 } from '@nestjs/common';
 import type { Response } from 'express';
-import moment from 'moment';
 import { Public } from 'src/infra/auth/decorators/public.decorator';
 import { Environment } from 'src/infra/environment/environment.service';
 import type { AsaasWebhookPayload } from 'src/modules/cobranca/interfaces/asaas-webhook-payload';
@@ -29,20 +28,30 @@ export class WebhookController {
     @Headers('asaas-access-token') asaasAccessToken: string,
     @Res() res: Response,
   ) {
-    if (asaasAccessToken !== this._environment.asaas.webhookAccessToken) {
-      throw new UnauthorizedException();
-    }
-
+    this._validarWebhookAccessToken(asaasAccessToken);
     const webhook = payload as AsaasWebhookPayload;
     if (webhook.event === 'PAYMENT_RECEIVED') {
       const payment = payload as AsaasWebhookPaymentPayload;
-      this._notificacaoPagamentoService.receber(payment.payment.id).subscribe({
-        next: () => res.status(204).send(),
-        error: (err: CustomError) =>
-          res.status(err.code).json({ status: 'error', message: err.message }),
-      });
+      this._receberPagamentoAsaas(payment, res);
     } else {
       res.status(204).send();
+    }
+  }
+
+  private _receberPagamentoAsaas(
+    payment: AsaasWebhookPaymentPayload,
+    res: Response<any, Record<string, any>>,
+  ) {
+    this._notificacaoPagamentoService.receber(payment.payment.id).subscribe({
+      next: () => res.status(204).send(),
+      error: (err: CustomError) =>
+        res.status(err.code).json({ status: 'error', message: err.message }),
+    });
+  }
+
+  private _validarWebhookAccessToken(asaasAccessToken: string) {
+    if (asaasAccessToken !== this._environment.asaas.webhookAccessToken) {
+      throw new UnauthorizedException();
     }
   }
 }
