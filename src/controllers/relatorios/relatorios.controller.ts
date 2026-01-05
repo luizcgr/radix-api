5de471c79cffdb05b1a5e9f3b817428acf4414e0
@luizcgr <luizcgr@gmail.com>
2026-01-05 01:09:09 +0000

    feat: update relatorios controller to include user info and adjust endpoint roles

--- src/controllers/relatorios/relatorios.controller.ts
+++ src/controllers/relatorios/relatorios.controller.ts
@@ -1,15 +1,17 @@
 import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { Roles } from 'src/infra/auth/decorators/roles.decorator';
+import { UserInfo } from 'src/infra/auth/user-info/user-info';
 import { RelatorioCelulaService } from 'src/modules/devolucao/services/relatorio-celula.service';
 
 @Controller('v1/relatorios')
 export class RelatoriosController {
   constructor(
+    private readonly _userInfo: UserInfo,
     private readonly _relatorioCelulaService: RelatorioCelulaService,
   ) {}
 
-  @Roles('celula')
-  @Get('celula/:celulaId/:mesReferencia/:anoReferencia/')
+  @Roles('setor', 'missao')
+  @Get('celulas/:celulaId/:mesReferencia/:anoReferencia/')
   gerarRelatorioCelula(
     @Param('celulaId', ParseIntPipe) celulaId: number,
     @Param('mesReferencia', ParseIntPipe) mesReferencia: number,
@@ -17,6 +19,19 @@ export class RelatoriosController {
   ) {
     return this._relatorioCelulaService.gerar({
       celulaId,
+      mesReferencia,
+      anoReferencia,
+    });
+  }
+
+  @Roles('celula')
+  @Get('minha-celula/:mesReferencia/:anoReferencia/')
+  gerarRelatorioMinhaCelula(
+    @Param('mesReferencia', ParseIntPipe) mesReferencia: number,
+    @Param('anoReferencia', ParseIntPipe) anoReferencia: number,
+  ) {
+    return this._relatorioCelulaService.gerar({
+      celulaId: this._userInfo.pessoa!.celula.id,
       mesReferencia,
       anoReferencia,
     });
